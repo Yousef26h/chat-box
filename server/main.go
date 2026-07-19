e154b106cee62df16224b594c6cbf55178ba78d5
Yousef <yousefsalem276@gmail.com>
2026-07-18 20:37:24 -0500

    remove embedded home page html

--- server/main.go
+++ server/main.go
@@ -11,15 +11,6 @@ type Hub struct {
 
 func main() {
 
-	// scanner := bufio.NewScanner(os.Stdin)
-
-	// fmt.Println("Enter text (Press Ctrl+C to stop):")
-
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	fmt.Printf("Captured: %s\n", line)
-	// }
-
 	// Optional: Serve a minimal HTML page to test the client locally
 	http.HandleFunc("/", serveHome)
 
@@ -44,35 +35,3 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 	tmpl.Execute(w, nil)
 }
-
-// Embedded HTML string for visual browser testing
-const htmlTestPage = `
-<!DOCTYPE html>
-<html>
-<head><title>Go Chat Test</title></head>
-<body>
-    <div id="output" style="height: 300px; overflow-y: scroll; border: 1px solid #ccc; padding: 10px;"></div>
-    <input type="text" id="input" placeholder="Type a message..." style="width: 80%;" />
-    <button onclick="send()">Send</button>
-
-    <script>
-        const ws = new WebSocket("ws://" + window.location.host + "/ws");
-        const output = document.getElementById("output");
-        const input = document.getElementById("input");
-
-        ws.onmessage = function(event) {
-            output.innerHTML += "<div>" + event.data + "</div>";
-            output.scrollTop = output.scrollHeight;
-        };
-
-        function send() {
-            if (input.value.trim() !== "") {
-                ws.send(input.value);
-                input.value = "";
-            }
-        }
-        input.addEventListener("keypress", function(e) { if(e.key === "Enter") send(); });
-    </script>
-</body>
-</html>
-`
